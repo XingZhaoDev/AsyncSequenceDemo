de4539d31141db09cec5002b4cc26397c43b21a1
Xing Zhao <zhaoxing8325@gmail.com>
2022-04-12 22:21:19 -0700

    Initial Commit

---
+++ AsyncSequenceDemo/AsyncSequenceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncSequenceDemoApp.swift
+//  AsyncSequenceDemo
+//
+//  Created by Xing Zhao on 2022-04-12.
+//
+
+import SwiftUI
+
+@main
+struct AsyncSequenceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncSequenceDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncSequenceDemo
+//
+//  Created by Xing Zhao on 2022-04-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
